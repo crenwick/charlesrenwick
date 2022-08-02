09925ca804362113606045ec7f84cd3f19993ced
Charles Renwick <charles.srenwick@gmail.com>
2022-08-02 09:24:55 -0400

    lazy-load video assets

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
-'use strict';
+"use strict";
 
-var myName = ' _____ _                _                  \n\/  __ \\ |              | |                 \n| \/  \\\/ |__   __ _ _ __| | ___  ___        \n| |   | \'_ \\ \/ _` | \'__| |\/ _ \\\/ __|       \n| \\__\/\\ | | | (_| | |  | |  __\/\\__ \\       \n \\____\/_| |_|\\__,_|_|  |_|\\___||___\/       \n\n______                   _      _    \n| ___ \\                 (_)    | |   \n| |_\/ \/___ _ ____      ___  ___| | __\n|    \/\/ _ \\ \'_ \\ \\ \/\\ \/ \/ |\/ __| |\/ \/\n| |\\ \\  __\/ | | \\ V  V \/| | (__|   < \n\\_| \\_\\___|_| |_|\\_\/\\_\/ |_|\\___|_|\\_\\';
+var myName =
+  " _____ _                _                  \n/  __ \\ |              | |                 \n| /  \\/ |__   __ _ _ __| | ___  ___        \n| |   | '_ \\ / _` | '__| |/ _ \\/ __|       \n| \\__/\\ | | | (_| | |  | |  __/\\__ \\       \n \\____/_| |_|\\__,_|_|  |_|\\___||___/       ";
 // console.log(myName);
+
+// Lazy-load the videos
+
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy-video"));
+
+  if ("IntersectionObserver" in window) {
+    var lazyVideoObserver = new IntersectionObserver(function (
+      entries,
+      _observer
+    ) {
+      entries.forEach(function (video) {
+        if (video.isIntersecting) {
+          for (var source in video.target.children) {
+            var videoSource = video.target.children[source];
+            if (
+              typeof videoSource.tagName === "string" &&
+              videoSource.tagName === "SOURCE"
+            ) {
+              videoSource.src = videoSource.dataset.src;
+            }
+          }
+
+          video.target.load();
+          video.target.classList.remove("lazy-video");
+          lazyVideoObserver.unobserve(video.target);
+        }
+      });
+    });
+
+    lazyVideos.forEach(function (lazyVideo) {
+      lazyVideoObserver.observe(lazyVideo);
+    });
+  }
+});
